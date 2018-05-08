2c37ddb91a91836cbfaebc22c6f496dc738a8315
HankBrownGit <hendrikbraun@fastmail.com>
2018-05-08 13:02:26 +0200

    trajectory added

--- destroyer.py
+++ destroyer.py
@@ -1,5 +1,47 @@
 import sys, pygame
 import datetime
+from math import sin, cos, radians, pi
+from time import sleep
+import math
+
+def project_point(original_x, original_y, bearing, distance):
+
+    if bearing >= 360:
+        bearing = bearing -360
+
+    if bearing == 0:
+        return [original_x, original_y - distance]
+    if bearing == 90:
+        return [original_x - distance, original_y]
+    if bearing == 180:
+        return [original_x, original_y + distance]
+    if bearing == 270:
+        return [original_x + distance, original_y]
+
+    if 0 <= bearing < 90:
+        angle = bearing
+        move_x = sin(radians(angle))*distance
+        move_y = cos(radians(angle))*distance
+        return [original_x + move_x, original_y - move_y]
+
+    if 90 <= bearing < 180:
+        angle = bearing - 90
+        move_x = cos(radians(angle))*distance
+        move_y = sin(radians(angle))*distance
+        return [original_x + move_x, original_y + move_y]
+
+    if 180 <= bearing < 270:
+        angle = bearing - 180
+        move_x = sin(radians(angle))*distance
+        move_y = cos(radians(angle))*distance
+        return [original_x - move_x, original_y + move_y]
+
+    if 270 <= bearing < 360:
+        angle = bearing - 270
+        move_x = cos(radians(angle))*distance
+        move_y = sin(radians(angle))*distance
+        return [original_x - move_x, original_y - move_y]
+
 
 class Destroyer(object):
     __tower_direction = 0
@@ -67,22 +109,33 @@ class Enemy(object):
 class Bullet(object):
     __type = None
     __power = None
-    __position = [None,None]
+    __position = []
     __direction = 0
-    __speed = 0
+    __old_time = None
+    __px_per_second = None
+    __original_time = None
 
-    def __init__(self, type, power, origin, direction, speed):
+    def __init__(self, type, power, origin, direction, px_per_second):
         self.__type = type
         self.__power = power
-        self.__position = origin
+        self.__position = list(origin)
         self.__direction = direction
-        self.__speed = speed
+        self.__old_time = datetime.datetime.now()
+        self.__px_per_second = px_per_second
+        self.__original_time = datetime.datetime.now()
 
     def move(self):
-        pass
+        new_time = datetime.datetime.now()
+        time_delta = new_time - self.__old_time
+        self.__old_time = new_time
+        vector_delta = math.floor(time_delta.total_seconds() * self.__px_per_second)
+        self.__position = project_point(self.__position[0], self.__position[1], self.__direction, vector_delta)
 
     def get_position(self):
-        return self.__position
+        return [int(math.floor(self.__position[0])), int(math.floor(self.__position[1]))]
+
+    def __del__(self):
+        print((datetime.datetime.now() - self.__original_time).total_seconds())
 
 class Bullets(object):
 
@@ -132,8 +185,26 @@ class Destroyer_logic(object):
         bullet_list = self.__bullets.get_bullets()
 
         for i in range(len(bullet_list)):
-            if (self.__window_size[0]>= bullet_list[i].get_position()[0] <= 0) or (self.__window_size[1] >= bullet_list[i].get_position()[1] <= 0):
+            if not (self.__window_size[0] >= bullet_list[i].get_position()[0] >= 0) or not (self.__window_size[1] >= bullet_list[i].get_position()[1] >= 0):
                 bullet_remove_list.append(i)
+
+        self.__bullets.remove_bullets(bullet_remove_list)
+
+class Destroyer_gfx(object):
+    __destroyer = None
+    __bullets = None
+    __enemies = None
+    __screen = None
+
+    def __init__(self, screen, destroyer, enemies, bullets):
+        self.__destroyer = destroyer
+        self.__enemies = enemies
+        self.__bullets = bullets
+        self.__screen = screen
+
+    def draw(self):
+        for b in self.__bullets.get_bullets():
+            pygame.draw.circle(self.__screen, (255,255,255), b.get_position(), 5,1)
 
 class Destroyer_game(object):
 
@@ -144,19 +215,25 @@ class Destroyer_game(object):
     __max_enemies = 1
     __window_size = ()
 
-    def __init__(self, window_size=(400,400), game_speed=1, max_enemies=1):
+    def __init__(self, window_size=(800,800), game_speed=1, max_enemies=1):
         self.__window_size = window_size
         self.__game_speed = game_speed
         self.__max_enemies = max_enemies
 
         pygame.init()
         screen = pygame.display.set_mode(window_size)
-        destroyer = Destroyer(0, 1000)
+        destroyer = Destroyer(0,1000)
         bullets = Bullets((window_size[0]/2, window_size[1]/2), window_size)
+        logic = Destroyer_logic(destroyer, None, bullets, window_size)
+        graphics = Destroyer_gfx(screen, destroyer, None, bullets)
 
         exit_game = False
 
         while not exit_game:
+
+            bullets.move()
+            logic.check()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: sys.exit()
 
@@ -167,18 +244,18 @@ class Destroyer_game(object):
                         exit_game = True
 
                     if key == "right":
-                        destroyer.turn_tower(1,1)
+                        destroyer.turn_tower(1,4)
 
                     if key == "left":
-                        destroyer.turn_tower(3,1)
+                        destroyer.turn_tower(3,4)
 
                     if key == "space":
                         if destroyer.shoot():
                             bullets.add_bullet(0,100, destroyer.get_direction(), 100)
 
-            bullets.move()
-
-
+            graphics.draw()
+            pygame.display.update()
+            sleep(0.01)
             #screen.fill(black)
             #screen.blit(ball, ballrect)
             #pygame.display.flip()
